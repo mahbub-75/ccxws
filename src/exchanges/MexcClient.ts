1b0288e8364cd7d854bf065a5a3279845ade917c
Mahbobeh khazaee <mahbub.khazaee@gmail.com>
2023-01-13 11:22:11 -0800

    fix Mexc Ticker websocket

--- src/exchanges/MexcClient.ts
+++ src/exchanges/MexcClient.ts
@@ -28,17 +28,18 @@ export class MexcClient extends BasicClient {
     protected _sendSubTicker (remote_id: string) {
         this._wss.send(
             JSON.stringify({
-                "method": "sub.tickers",
-                "param": {}
+                method: "sub.ticker",
+                param: {
+                    symbol: remote_id,
+                }
             }),
         );
     };
     protected _sendUnsubTicker (remote_id: string) {
         this._wss.send(
             JSON.stringify({
-                "method": "unsub.tickers",
-                "param": {}
-
+                method: "unsub.ticker",
+                param: {}
             }),
         );
     };
